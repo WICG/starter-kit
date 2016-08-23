e1fcad6f210b8b1f988cc8390e60d5a0070293a5
Marcos Caceres <marcos@marcosc.com>
2016-08-23 19:24:10 +1000

    feat(git): teach git more common tasks

--- git.js
+++ git.js
@@ -1,9 +1,11 @@
 "use strict";
 const exec = require("child_process").exec;
+const path = require("path");
+const async = require("marcosc-async");
 
 function toExecPromise(cmd, timeout) {
   if (!timeout) {
-    timeout = 40000;
+    timeout = 60000;
   }
   return new Promise((resolve, reject) => {
     const id = setTimeout(() => {
@@ -23,5 +25,48 @@ function toExecPromise(cmd, timeout) {
 function git(cmd) {
   return toExecPromise(`git ${cmd}`);
 }
+
+git.getCurrentBranch = async(function*() {
+  const branch = yield git(`rev-parse --abbrev-ref HEAD`);
+  return branch.trim();
+});
+
+git.getConfigData = async(function*(configItem) {
+  let data;
+  try {
+    data = yield git(configItem);
+  } catch (err) {
+    data = "";
+  }
+  return data;
+});
+
+git.getBranches = async(function*() {
+  const rawBranches = yield git("branch --no-color");
+  const branches = rawBranches
+    .split("\n")
+    .map(branch => branch.replace("*", "").trim())
+    .reduce((collector, branch) => collector.add(branch), new Set());
+  return Array.from(branches);
+});
+
+git.hasBranch = async(function*(branch) {
+  const branches = yield git.getBranches();
+  return branches.includes(branch);
+});
+
+git.switchBranch = async(function*(branch) {
+  const hasBranch = yield git.hasBranch(branch);
+  if (!hasBranch) {
+    yield git(`checkout -b ${branch}`);
+  } else {
+    yield git(`checkout ${branch}`);
+  }
+});
+
+git.getRepoName = async(function*() {
+  const name = yield git("rev-parse --show-toplevel");
+  return path.basename(name).trim();
+});
 
 module.exports = git;
