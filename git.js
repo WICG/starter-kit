ece1ef9e42a0ac254e8fdbb68f175e6a116df20a
Marcos Caceres <marcos@marcosc.com>
2017-05-01 14:57:29 +1000

    refactor: use async/await

--- git.js
+++ git.js
@@ -1,17 +1,19 @@
 "use strict";
 const exec = require("child_process").exec;
 const path = require("path");
-const async = require("marcosc-async");
 
 function toExecPromise(cmd, timeout) {
   if (!timeout) {
     timeout = 60000;
   }
   return new Promise((resolve, reject) => {
-    const id = setTimeout(() => {
-      reject(new Error(`Command took too long: ${cmd}`));
-      proc.kill("SIGTERM");
-    }, timeout);
+    const id = setTimeout(
+      () => {
+        reject(new Error(`Command took too long: ${cmd}`));
+        proc.kill("SIGTERM");
+      },
+      timeout
+    );
     const proc = exec(cmd, (err, stdout) => {
       clearTimeout(id);
       if (err) {
@@ -26,47 +28,47 @@ function git(cmd) {
   return toExecPromise(`git ${cmd}`);
 }
 
-git.getCurrentBranch = async(function*() {
-  const branch = yield git(`rev-parse --abbrev-ref HEAD`);
+git.getCurrentBranch = async () => {
+  const branch = await git(`rev-parse --abbrev-ref HEAD`);
   return branch.trim();
-});
+};
 
-git.getConfigData = async(function*(configItem) {
+git.getConfigData = async configItem => {
   let data;
   try {
-    data = yield git(configItem);
+    data = await git(configItem);
   } catch (err) {
     data = "";
   }
   return data;
-});
+};
 
-git.getBranches = async(function*() {
-  const rawBranches = yield git("branch --no-color");
+git.getBranches = async () => {
+  const rawBranches = await git("branch --no-color");
   const branches = rawBranches
     .split("\n")
     .map(branch => branch.replace("*", "").trim())
     .reduce((collector, branch) => collector.add(branch), new Set());
   return Array.from(branches);
-});
+};
 
-git.hasBranch = async(function*(branch) {
-  const branches = yield git.getBranches();
+git.hasBranch = async branch => {
+  const branches = await git.getBranches();
   return branches.includes(branch);
-});
+};
 
-git.switchBranch = async(function*(branch) {
-  const hasBranch = yield git.hasBranch(branch);
+git.switchBranch = async branch => {
+  const hasBranch = await git.hasBranch(branch);
   if (!hasBranch) {
-    yield git(`checkout -b ${branch}`);
+    await git(`checkout -b ${branch}`);
   } else {
-    yield git(`checkout ${branch}`);
+    await git(`checkout ${branch}`);
   }
-});
+};
 
-git.getRepoName = async(function*() {
-  const name = yield git("rev-parse --show-toplevel");
+git.getRepoName = async () => {
+  const name = await git("rev-parse --show-toplevel");
   return path.basename(name).trim();
-});
+};
 
 module.exports = git;
